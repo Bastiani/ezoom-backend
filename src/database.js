1eb8da00df082ec4d8fc69e6926e0a4784985801
Bastiani <rafacdb@gmail.com>
2018-11-19 10:22:34 -0200

    first commit

--- src/database.js
+++ src/database.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
 // @flow
-import * as path from 'path';
-
 import Sequelize from 'sequelize';
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.resolve(`${__dirname}/databaseConfig.json`))[env];
+const config = require('./databaseConfig.json')[env];
 
 export function createSequelize() {
   return new Sequelize(config.database, config.username, config.password, {
